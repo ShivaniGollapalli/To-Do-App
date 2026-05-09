e78e04ab742407ce5cdee6c906c497f2039b679b
ShivaniGollapalli <shivanigollapalli73@gmail.com>
2026-05-09 14:27:18 +0530

    Added Versal Origin to Allow

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ import taskRouter from "./routes/taskRouter.js";
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
-const allowedOrigins = ["http://localhost:5173"];
-app.use(express.json());
+const allowedOrigins = ["http://localhost:5173",'https://to-do-app-eta-five.vercel.app'];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
-app.get("/", (req, res) => res.send("API Working Shivani"));
+app.use(express.json());
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
 app.use("/task", taskRouter);
 app.listen(port, () => {
   console.log(`🚀 Server is running on ${port}`);
